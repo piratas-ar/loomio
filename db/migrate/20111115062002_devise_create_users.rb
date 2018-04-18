73e6fea5b4c66172a2b4a9a2e65775fd1fa37cee
fauno <fauno@endefensadelsl.org>
2018-04-18 18:39:25 -0300

    no trackear usuarias

--- db/migrate/20111115062002_devise_create_users.rb
+++ db/migrate/20111115062002_devise_create_users.rb
@@ -12,13 +12,6 @@ class DeviseCreateUsers < ActiveRecord::Migration
       ## Rememberable
       t.datetime :remember_created_at
 
-      ## Trackable
-      t.integer  :sign_in_count, :default => 0
-      t.datetime :current_sign_in_at
-      t.datetime :last_sign_in_at
-      t.string   :current_sign_in_ip
-      t.string   :last_sign_in_ip
-
       ## Encryptable
       # t.string :password_salt
 
